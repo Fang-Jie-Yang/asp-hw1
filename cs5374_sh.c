c2d9962f2a2203df4babb3aacef17b1d0c0936bb
r12922015 <r12922015@ntu.edu.tw>
2024-02-26 06:32:21 +0000

    init

---
+++ cs5374_sh.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// TODO: Ctrl+c signal handler
+
+	// main loop
+	while(1) {
+		// get a "line" of input
+
+		// split into cmd by "|"
+
+		// for each cmd, split by " "
+
+			// handle cmd+arg
+
+	}
+
+	return 0;
+}
