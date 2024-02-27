22dd8088f7176bcd9689b75c3fbc0feb6c0add73
r12922015 <r12922015@ntu.edu.tw>
2024-02-27 06:18:38 +0000

    basic impl for command parsing

--- command.c
+++ command.c
@@ -2,20 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "command.h"
 
-struct command command_list_cache[] = NULL;
+struct command **command_list_cache = NULL;
 
-static void command_parse() {
+// parse a space-separated string into struct command
+static struct command *command_parse(char *s) {
+
+	char *token;
+	int argc = 0;
+	struct command *res;
+
+	token = strtok(s, COMMAND_DELIM);
+	if (token == NULL) {
+		// an empty command
+		//fprintf(stderr, "error: parsing error\n");
+		return NULL;
+	}
+
+	res = (struct command *)malloc(sizeof(struct command) + sizeof(char *) * _POSIX_ARG_MAX);
+	if (res == NULL) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return NULL;
+	}
+	res->pathname = token;
+
+	while (token != NULL) {
+		fprintf(stderr, "-- debug: %s\n", token);
+		res->argv[argc] = token;
+		argc++;
+
+		if (argc >= _POSIX_ARG_MAX) {
+			fprintf(stderr, "error: too many arguments\n");
+			return NULL;
+		}
+
+		token = strtok(NULL, COMMAND_DELIM);
+	}
+
+	res->argv[argc] = (char *)NULL;
+
+	return res;
 }
+
 // the number of entries in the returned array
 // will be limited by _POSIX_ARG_MAX
 // return a null-terminated list
-struct command command_list_parse[](const char *s) {
+struct command **command_list_parse(char *s) {
 
-	char *token;
+	char *sub;
 	int cnt = 0;
+	struct command *cmd;
 
 	if (command_list_cache == NULL) {
 		command_list_cache = malloc(sizeof(struct command *) * _POSIX_ARG_MAX);
@@ -24,17 +63,76 @@ struct command command_list_parse[](const char *s) {
 		}
 	}
 
-	token = strtok(s, COMMAND_PIPE);
-	while (token != NULL) {
-		// XXX: double pipe? "a || b"
-		fprintf(stderr, "debug: %s\n", token);
-		
-		token = strtok(NULL, COMMAND_PIPE);
+	if (*s == '\0') {
+		return NULL;
 	}
 
+	// use strsep because we don't allow contiguous '|'
+	sub = strsep(&s, COMMAND_PIPE);
+	while (sub != NULL) {
+		fprintf(stderr, "debug: %s\n", sub);
+		cmd = command_parse(sub);
+
+		if (cmd == NULL) {
+			// we encounter a empty command
+			fprintf(stderr, "error: parsing error\n");
+			return NULL;
+		}
+
+		command_list_cache[cnt] = cmd;
+		cnt++;
+
+		if (cnt >= _POSIX_ARG_MAX) {
+			fprintf(stderr, "error: too many arguments\n");
+			return NULL;
+		}
+		sub = strsep(&s, COMMAND_PIPE);
+	}
+	command_list_cache[cnt] = (struct command *)NULL;
 
 	return command_list_cache;
 }
 
 
-void command_free(struct command *list[]);
+void command_free(struct command *list[]) {
+	return;
+}
+
+void command_print(struct command *cmd) {
+
+	int i;
+
+	if (cmd == NULL) {
+		return;
+	}
+
+	if (cmd->pathname == NULL) {
+		fprintf(stderr, "==== NULL pathname!\n");
+		return;
+	}
+	fprintf(stderr, "==== pathname: %s\n", cmd->pathname);
+	for (i = 0; ; i++) {
+		if (cmd->argv[i] == NULL) {
+			break;
+		}
+		fprintf(stderr, "==== argv    : %s\n", cmd->argv[i]);
+	}
+	return;
+}
+
+void command_list_print(struct command **cmd_list) {
+
+	int i;
+
+	if (cmd_list == NULL) {
+		return;
+	}
+
+	for (i = 0; ; i++) {
+		if (cmd_list[i] == NULL) {
+			break;
+		}
+		command_print(cmd_list[i]);
+	}
+	return;
+}
--- command.h
+++ command.h
@@ -1,8 +1,8 @@
 #ifndef COMMAND_H
 #define COMMAND_H 
 
-#define COMMAND_PIPE  '|'
-#define COMMAND_DELIM ' '
+#define COMMAND_PIPE  "|"
+#define COMMAND_DELIM " "
 
 struct command {
 	char *pathname;
@@ -12,12 +12,14 @@ struct command {
 };
 
 // the "cache" for storing parsing outcome
-// some we don't have to realloc everytime
-extern struct command command_list_cache[];
+// some we don't have to alloc everytime
+extern struct command **command_list_cache;
 
 // the number of entries in the returned list
 // will be limited by _POSIX_ARG_MAX
-struct command *command_parse[](const char *s);
+struct command **command_list_parse(char *s);
 void command_free(struct command *list[]);
+void command_print(struct command *cmd);
+void command_list_print(struct command **cmd_list);
 
 #endif
