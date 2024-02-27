c8d73d9d90d8f700ffe5f641f6152e385d8b3a8f
r12922015 <r12922015@ntu.edu.tw>
2024-02-27 06:19:57 +0000

    basic structure for the shell

---
+++ Makefile
@@ -0,0 +1,10 @@
+
+command: command.h command.c
+	gcc -c -o command.o command.c
+
+history: history.h history.c
+	gcc -c -o history.o history.c
+
+shell: shell.c history command
+	gcc -o cs5374_sh shell.c history.o command.o
+
--- shell.c
+++ shell.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "history.h"
+#include "command.h"
 
 int main(void) {
+
+	char *input = NULL;
+	size_t len = 0;
+	ssize_t read_len;
+	struct history *hist;
+	struct command **cmd_list;
+	int i;
+
 	// TODO: Ctrl+c signal handler
 
-	// main loop
-	while(1) {
-		// get a "line" of input
+	// init history
+	hist = history_new();
 
-		// split into cmd by "|"
+	// main loop
+	while (1) {
+
+		putchar('$');
+		// get a "line" of input
+		read_len = getline(&input, &len, stdin);
+		if (read_len < 0) {
+			fprintf(stderr, "error: %s\n", strerror(errno));
+			// XXX: continue or break?
+			continue;
+		}
+		// remove trailing newline
+		if (input[read_len - 1] == '\n') {
+			input[read_len - 1] = '\0';
+		}
+		history_push(hist, input);
+
+		// TODO: split line into cmds by "|"
+		cmd_list = command_list_parse(input);
 
 		// for each cmd, split by " "
-
 			// handle cmd+arg
+		command_list_print(cmd_list);
+		history_show(hist, 10);
 
 	}
 
