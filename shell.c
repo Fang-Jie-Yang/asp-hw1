62c0dd1b6c4fae22d32a9a87329d95fd6bea4fb4
r12922015 <r12922015@ntu.edu.tw>
2024-02-28 01:50:06 +0000

    add pipe utils and update flow

--- shell.c
+++ shell.c
@@ -2,10 +2,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include "history.h"
 #include "command.h"
 #include "list.h"
+
+// return -1 on error
+static inline int pipe_make(struct command *cmd, struct command *next) {
+
+	int fds[2];
+
+	if (pipe(fds) == -1) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return -1;
+	}
+
+	cmd->pipe_fd[1]  = fds[1];
+	next->pipe_fd[0] = fds[0];
+
+	cmd->unused_fd[0]  = fds[0];
+	next->unused_fd[1] = fds[1];
+
+	return 0;
+}
+
+static inline void pipe_close(int *pipefd) {
+	if (*pipefd == -1)
+		return;
+	if (close(*pipefd) == -1) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		// XXX: I think we have to abort in this case
+		// TODO: free resources
+		exit(-1);
+	}
+	*pipefd = -1;
+}
+
+// return -1 on error, *pipefd is closed either success or fail
+static inline int pipe_dup2(int *pipefd, int newfd) {
+	int ret;
+	if (*pipefd == -1)
+		return 0;
+	ret = dup2(*pipefd, newfd);
+	if (ret == -1) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+	}
+	pipe_close(pipefd);
+	return ret;
+}
+
+static inline int do_command(struct command *cmd) {
+
+	pid_t pid;
+
+	pid = fork();
+	if(pid == -1){
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return -1;
+	}
+
+	// child
+	if(pid == 0){
+		if (pipe_dup2(&cmd->pipe_fd[0], STDOUT_FILENO)) {
+			exit(-1);
+		}
+		if (pipe_dup2(&cmd->pipe_fd[1], STDIN_FILENO)) {
+			exit(-1);
+		}
+		pipe_close(&cmd->unused_fd[0]);
+		pipe_close(&cmd->unused_fd[1]);
+		execlp("ls","ls",NULL);
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	// parent
+	pipe_close(&cmd->pipe_fd[0]);
+	pipe_close(&cmd->pipe_fd[1]);
+	return 0;
+}
 
 int main(void) {
 
@@ -13,23 +89,19 @@ int main(void) {
 	size_t len = 0;
 	ssize_t read_len;
 	struct history *hist;
-	LIST_HEAD(cmd_list);
-	int i;
+	LIST_HEAD(cmd_list_head);
+	struct list_head *cmd;
 
 	// TODO: Ctrl+c signal handler
 
-	// init history
 	hist = history_new();
 
-	// main loop
 	while (1) {
 
 		putchar('$');
-		// get a "line" of input
 		read_len = getline(&input, &len, stdin);
 		if (read_len < 0) {
 			fprintf(stderr, "error: %s\n", strerror(errno));
-			// XXX: continue or break?
 			continue;
 		}
 		// remove trailing newline
@@ -38,16 +110,24 @@ int main(void) {
 		}
 		history_push(hist, input);
 
-		command_list_parse(&cmd_list, input);
-		if (list_empty(&cmd_list)) {
+		command_list_parse(&cmd_list_head, input);
+		if (list_empty(&cmd_list_head)) {
 			continue;
 		}
 
-		command_list_print(&cmd_list);
-		// for each cmd
-			// handle cmd+arg
+		command_list_print(&cmd_list_head);
+		list_for_each(cmd, &cmd_list_head) {
+			if (cmd->next != &cmd_list_head) {
+				if (pipe_make((struct command *)cmd, (struct command *)cmd->next)) {
+					break;
+				}
+			}
+			if (do_command((struct command *)cmd)) {
+				break;
+			}
+		}
 
-		command_list_free(&cmd_list);
+		command_list_free(&cmd_list_head);
 		history_show(hist, 10);
 
 	}
